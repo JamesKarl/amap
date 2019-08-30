1c5f2308de0736039aa3a81e72a27acf0501e4e5
柯军 <kej@meiyibao.com>
2019-08-30 16:17:19 +0800

    code refactor.

--- android/src/main/kotlin/com/jameskarl/amap/map/MapMessageHandler.kt
+++ android/src/main/kotlin/com/jameskarl/amap/map/MapMessageHandler.kt
@@ -2,8 +2,6 @@ package com.jameskarl.amap.map
 
 import android.util.Log
 import com.amap.api.maps.TextureMapView
-import com.amap.api.maps.model.LatLng
-import com.amap.api.maps.model.MarkerOptions
 import com.jameskarl.amap.AmapPlugin
 import com.jameskarl.amap.map.bean.ChannelMessageData
 import com.jameskarl.amap.map.bean.toMarkerData
@@ -27,19 +25,19 @@ class MapMessageHandler(
         messageChannel.setMessageHandler(this)
         mapView.map.setOnMarkerClickListener {
             it.showInfoWindow()
-            sendMessageToFlutter(ChannelMessageData(MapMethods.onMarkerClicked, it.toMarkerData()))
+            sendJsonMessageToFlutter(ChannelMessageData(MapMethods.onMarkerClicked, it.toMarkerData()))
             true
         }
         mapView.map.setOnMapClickListener {
-            sendMessageToFlutter(ChannelMessageData(MapMethods.onMapClicked, it))
+            sendJsonMessageToFlutter(ChannelMessageData(MapMethods.onMapClicked, it))
         }
         mapView.map.setOnMapLoadedListener {
-            sendMessageToFlutter(ChannelMessageData(MapMethods.onMapLoaded))
+            sendJsonMessageToFlutter(ChannelMessageData(MapMethods.onMapLoaded))
         }
         Log.d("MAP", mapMethodChannelName)
     }
 
-    private fun sendMessageToFlutter(message: ChannelMessageData) {
+    private fun sendJsonMessageToFlutter(message: ChannelMessageData) {
         val msg = message.toJson()
         Log.d("MAP", msg)
         messageChannel.send(msg)
@@ -52,63 +50,17 @@ class MapMessageHandler(
                 val methodId = json["id"]
                 val data = json.opt("data")
                 if (methodId is String && methodId.isNotBlank()) {
-                    handle(methodId, data, reply)
+                    MapMethods.handleMessage(mapView.map, methodId, data, reply)
                 } else {
                     Log.e("MAP", "method id is absent.")
-                    commonReply(methodId?.toString() ?: "???", false, "method id is absent", reply)
+                    MapMethods.commonReply(methodId?.toString()
+                            ?: "???", false, "method id is absent", reply)
                 }
             } catch (e: Throwable) {
                 e.printStackTrace()
-                commonReply("???", false, e.localizedMessage, reply)
+                MapMethods.commonReply("???", false, e.localizedMessage, reply)
             }
         }
     }
 
-    private fun handle(methodId: String, data: Any?, reply: BasicMessageChannel.Reply<String>) {
-
-        when (methodId) {
-            MapMethods.getCenter -> getCenterPoint(reply)
-            MapMethods.addMarker -> {
-                if (data is JSONObject)
-                    addMarker(data, reply)
-                else
-                    commonReply(methodId, false, "参数错误", reply)
-            }
-        }
-    }
-
-    private fun addMarker(data: JSONObject, reply: BasicMessageChannel.Reply<String>) {
-        try {
-            val pos = data.getJSONObject("at")
-            val markerData = data.getJSONObject("marker")
-            val marker = MarkerOptions()
-                    .position(LatLng(pos.getDouble("latitude"), pos.getDouble("longitude")))
-                    .title(markerData.getString("title"))
-                    .snippet(markerData.getString("snippet"))
-            mapView.map.addMarker(marker)
-        } catch (e: Throwable) {
-            e.printStackTrace()
-        }
-    }
-
-    private fun getCenterPoint(reply: BasicMessageChannel.Reply<String>) {
-        val target = mapView.map.cameraPosition.target
-        val data = mapOf(
-                "id" to MapMethods.getCenter,
-                "data" to mapOf(
-                        "longitude" to target.longitude,
-                        "latitude" to target.latitude
-                )
-        )
-        reply.reply(data.toJson())
-    }
-
-    private fun commonReply(methodId: String, success: Boolean, message: String?, reply: BasicMessageChannel.Reply<String>) {
-        val data = mapOf(
-                "id" to methodId,
-                "success" to success,
-                "message" to (message ?: "")
-        )
-        reply.reply(data.toJson())
-    }
 }
--- android/src/main/kotlin/com/jameskarl/amap/map/MapMethods.kt
+++ android/src/main/kotlin/com/jameskarl/amap/map/MapMethods.kt
@@ -1,11 +1,69 @@
 package com.jameskarl.amap.map
 
+import com.amap.api.maps.AMap
+import com.amap.api.maps.model.LatLng
+import com.amap.api.maps.model.MarkerOptions
+import com.jameskarl.shop.toJson
+import io.flutter.plugin.common.BasicMessageChannel
+import org.json.JSONObject
+
 object MapMethods {
     const val onMapLoaded = "onMapLoaded"
 
     const val onMapClicked = "onMapClicked"
     const val onMarkerClicked = "onMarkerClicked"
 
-    const val addMarker = "addMarker"
-    const val getCenter = "getCenter"
+    private const val addMarker = "addMarker"
+    private const val getCenter = "getCenter"
+
+
+    fun handleMessage(map: AMap, methodId: String, data: Any?, reply: BasicMessageChannel.Reply<String>) {
+
+        when (methodId) {
+            MapMethods.getCenter -> MapMethods.getCenterPoint(map, reply)
+            MapMethods.addMarker -> {
+                if (data is JSONObject)
+                    MapMethods.addMarker(map, data, reply)
+                else
+                    commonReply(methodId, false, "参数错误", reply)
+            }
+        }
+    }
+
+    fun commonReply(methodId: String, success: Boolean, message: String?, reply: BasicMessageChannel.Reply<String>) {
+        val data = mapOf(
+                "id" to methodId,
+                "success" to success,
+                "message" to (message ?: "")
+        )
+        reply.reply(data.toJson())
+    }
+
+
+    private fun addMarker(map: AMap, data: JSONObject, reply: BasicMessageChannel.Reply<String>) {
+        try {
+            val pos = data.getJSONObject("at")
+            val markerData = data.getJSONObject("marker")
+            val marker = MarkerOptions()
+                    .position(LatLng(pos.getDouble("latitude"), pos.getDouble("longitude")))
+                    .title(markerData.getString("title"))
+                    .snippet(markerData.getString("snippet"))
+            map.addMarker(marker)
+        } catch (e: Throwable) {
+            e.printStackTrace()
+        }
+    }
+
+
+    private fun getCenterPoint(map: AMap, reply: BasicMessageChannel.Reply<String>) {
+        val target = map.cameraPosition.target
+        val data = mapOf(
+                "id" to MapMethods.getCenter,
+                "data" to mapOf(
+                        "longitude" to target.longitude,
+                        "latitude" to target.latitude
+                )
+        )
+        reply.reply(data.toJson())
+    }
 }
