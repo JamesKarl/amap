57660575a05bafa937aaec42f3c615308756185b
柯军 <kej@meiyibao.com>
2019-09-01 07:59:17 +0800

    marker icon support cache.

--- android/src/main/kotlin/com/jameskarl/amap/map/apis/MarkerApi.kt
+++ android/src/main/kotlin/com/jameskarl/amap/map/apis/MarkerApi.kt
@@ -1,8 +1,12 @@
 package com.jameskarl.amap.map.apis
 
 import android.graphics.BitmapFactory
+import android.util.Log
+import androidx.collection.LruCache
 import com.amap.api.maps.AMap
+import com.amap.api.maps.model.BitmapDescriptor
 import com.amap.api.maps.model.BitmapDescriptorFactory
+import com.amap.api.maps.model.MarkerOptions
 import com.jameskarl.amap.map.PlatformMapView
 import com.jameskarl.amap.map.ReplyToFlutter
 import com.jameskarl.amap.map.bean.MarkerOptionData
@@ -10,6 +14,7 @@ import com.jameskarl.amap.map.parseObject
 import org.json.JSONObject
 
 class MarkerApi {
+    private val cache: LruCache<String, BitmapDescriptor> = LruCache(20)
     fun addMarker(map: AMap, data: Any?): ReplyToFlutter {
         require(data is JSONObject)
         return try {
@@ -17,22 +22,37 @@ class MarkerApi {
             if (markerOptionData == null) {
                 ReplyToFlutter.Failed(message = "parse data failed. $data")
             } else {
-                PlatformMapView.infoWindowAdapter?.let {
-                    map.setInfoWindowAdapter(it)
-                }
-
+                setInfoWindowAdapter(map)
                 val markerOptions = markerOptionData.toMarkerOptions()
-                markerOptionData.icon?.let { icon ->
-                    PlatformMapView.getFlutterAsset(icon)?.let { fd ->
-                        val bitmap = BitmapFactory.decodeStream(fd.createInputStream())
-                        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(bitmap))
-                    }
-                }
+                attachMarkerIcon(markerOptionData, markerOptions)
                 map.addMarker(markerOptions)
                 ReplyToFlutter.Success()
             }
         } catch (e: Throwable) {
             ReplyToFlutter.Failed(message = e.localizedMessage)
+        }
+    }
+
+    private fun setInfoWindowAdapter(map: AMap) {
+        PlatformMapView.infoWindowAdapter?.let {
+            map.setInfoWindowAdapter(it)
+        }
+    }
+
+    private fun attachMarkerIcon(markerOptionData: MarkerOptionData, markerOptions: MarkerOptions) {
+        markerOptionData.icon?.let { icon ->
+            val cachedBitmapDescriptor = cache.get(icon)
+            if (cachedBitmapDescriptor != null) {
+                markerOptions.icon(cachedBitmapDescriptor)
+            } else {
+                PlatformMapView.getFlutterAsset(icon)?.let { fd ->
+                    val bitmap = BitmapFactory.decodeStream(fd.createInputStream())
+                    val bitmapDescriptor = BitmapDescriptorFactory.fromBitmap(bitmap)
+                    cache.put(icon, bitmapDescriptor)
+                    Log.d("MAP", "create new marker icon identified by $icon")
+                    markerOptions.icon(bitmapDescriptor)
+                }
+            }
         }
     }
 }
