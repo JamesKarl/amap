656860d13bcc9beda3937e4313b2ea64cadb00d5
柯军 <kej@meiyibao.com>
2019-08-31 15:05:06 +0800

    add some methods

--- lib/src/mixin/map_info.dart
+++ lib/src/mixin/map_info.dart
@@ -20,4 +20,29 @@ mixin MapInfoMixin on NativeMessenger {
   Future<MessageReply> setZoomControlsEnabled(bool enable) {
     return sendMessageToNative(MapMethods.setZoomControlsEnabled, data: enable);
   }
+
+  Future<MessageReply> setCompassEnabled(bool enable) {
+    return sendMessageToNative(MapMethods.setCompassEnabled, data: enable);
+  }
+
+  Future<MessageReply> setMyLocationButtonEnabled(bool enable) {
+    return sendMessageToNative(MapMethods.setMyLocationButtonEnabled,
+        data: enable);
+  }
+
+  Future<MessageReply> setMyLocationEnabled(bool enable) {
+    return sendMessageToNative(MapMethods.setMyLocationEnabled, data: enable);
+  }
+
+  Future<MessageReply> setScaleControlsEnabled(bool enable) {
+    return sendMessageToNative(MapMethods.setScaleControlsEnabled,
+        data: enable);
+  }
+
+  Future<MessageReply> setLogoPosition(LogoPosition position) {
+    return sendMessageToNative(MapMethods.setLogoPosition,
+        data: position.index);
+  }
 }
+
+enum LogoPosition { BOTTOM_LEFT, BOTTOM_CENTER, BOTTOM_RIGHT }
