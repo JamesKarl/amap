717afb2d4e8893057e02d5f1f0000a51e91ad455
柯军 <kej@meiyibao.com>
2019-08-31 10:35:48 +0800

    add toJson and fromJson

--- lib/src/amap/AMap.dart
+++ lib/src/amap/AMap.dart
@@ -42,4 +42,40 @@ class UiSettings {
     this.zoomGesturesEnabled,
     this.allGesturesEnabled,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'logoPosition': this.logoPosition,
+      'zoomPosition': this.zoomPosition,
+      'compassEnabled': this.compassEnabled,
+      'gestureScaleByMapCenter': this.gestureScaleByMapCenter,
+      'indoorSwitchEnabled': this.indoorSwitchEnabled,
+      'myLocationButtonEnabled': this.myLocationButtonEnabled,
+      'rotateGesturesEnabled': this.rotateGesturesEnabled,
+      'scaleControlsEnabled': this.scaleControlsEnabled,
+      'scrollGesturesEnabled': this.scrollGesturesEnabled,
+      'tiltGesturesEnabled': this.tiltGesturesEnabled,
+      'zoomControlsEnabled': this.zoomControlsEnabled,
+      'zoomGesturesEnabled': this.zoomGesturesEnabled,
+      'allGesturesEnabled': this.allGesturesEnabled,
+    };
+  }
+
+  factory UiSettings.fromJson(Map<String, dynamic> map) {
+    return new UiSettings(
+      logoPosition: map['logoPosition'] as int,
+      zoomPosition: map['zoomPosition'] as int,
+      compassEnabled: map['compassEnabled'] as bool,
+      gestureScaleByMapCenter: map['gestureScaleByMapCenter'] as bool,
+      indoorSwitchEnabled: map['indoorSwitchEnabled'] as bool,
+      myLocationButtonEnabled: map['myLocationButtonEnabled'] as bool,
+      rotateGesturesEnabled: map['rotateGesturesEnabled'] as bool,
+      scaleControlsEnabled: map['scaleControlsEnabled'] as bool,
+      scrollGesturesEnabled: map['scrollGesturesEnabled'] as bool,
+      tiltGesturesEnabled: map['tiltGesturesEnabled'] as bool,
+      zoomControlsEnabled: map['zoomControlsEnabled'] as bool,
+      zoomGesturesEnabled: map['zoomGesturesEnabled'] as bool,
+      allGesturesEnabled: map['allGesturesEnabled'] as bool,
+    );
+  }
 }
