7e996d0a894cb250aebef13222fd6672b74d17f9
柯军 <kej@meiyibao.com>
2019-09-08 14:52:24 +0800

    example code

--- example/lib/repository/apis.dart
+++ example/lib/repository/apis.dart
@@ -1,8 +1,8 @@
 import 'package:amap_example/repository/http_utils.dart';
 
 const bussApis = Apis(
-  "",
+  "http://test.store.meiyibao.com:8001/mobile/",
   "http://192.168.11.156:9090/apis/",
-  "",
+  "4a8f191bb0914baf8907f38881024f7b",
   true,
 );
--- example/lib/stories/transport/transport_main_page.dart
+++ example/lib/stories/transport/transport_main_page.dart
@@ -57,7 +57,7 @@ class _TransportMainPageState extends State<TransportMainPage>
                   Text(
                     "联系我们",
                     style:
-                    TextStyle(color: const Color(0xffFA8C16), fontSize: 15),
+                        TextStyle(color: const Color(0xffFA8C16), fontSize: 15),
                   ),
                 ],
               ),
