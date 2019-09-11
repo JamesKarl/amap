7fc3ac51b714945b3889739e037f96665b7ac480
James Karl <jameskarl2@163.com>
2019-09-11 11:27:24 +0800

    iOS: message channel works.

--- ios/Classes/SwiftAmapPlugin.swift
+++ ios/Classes/SwiftAmapPlugin.swift
@@ -2,13 +2,16 @@ import Flutter
 import UIKit
 
 public class SwiftAmapPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "amap", binaryMessenger: registrar.messenger())
-    let instance = SwiftAmapPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    static let mapViewType = "com.jameskarl/mapView"
+    private static let channelName = "com.jameskarl/map"
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: channelName, binaryMessenger: registrar.messenger())
+        let instance = SwiftAmapPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result("iOS " + UIDevice.current.systemVersion)
+    }
 }
