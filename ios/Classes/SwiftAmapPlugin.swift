66356d14dc974c5ea389fd8ce219eb5ef71fe3e2
James Karl <jameskarl2@163.com>
2020-04-23 18:11:43 +0800

    code refactor

--- ios/Classes/SwiftAmapPlugin.swift
+++ ios/Classes/SwiftAmapPlugin.swift
@@ -9,21 +9,14 @@ public class SwiftAmapPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
         AMapServices.shared().apiKey = "95dd02f71afb8c8fdeb4273ce402edce"
     
-        let channel = FlutterMethodChannel(name: channelName, binaryMessenger: registrar.messenger())
-        let instance = SwiftAmapPlugin()
-        registrar.addMethodCallDelegate(instance, channel: channel)
+        registrar.addMethodCallDelegate(
+            SwiftAmapPlugin(),
+            channel: FlutterMethodChannel(
+                name: channelName,
+                binaryMessenger: registrar.messenger()
+            )
+        )
         
-        let mapViewFactory = MapViewFactory(registrar: registrar)
-        registrar.register(mapViewFactory, withId: mapViewType)
-    }
-    
-    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        switch call.method {
-        case "getPlatformVersion":
-            result("iOS " + UIDevice.current.systemVersion)
-        default:
-            result(FlutterMethodNotImplemented)
-        }
-        
+        registrar.register(MapViewFactory(registrar: registrar), withId: mapViewType)
     }
 }
